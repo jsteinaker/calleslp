f360d765e1191bf7196d7e7d38b41d53896680cd
Javier Steinaker <jsteinaker@gmail.com>
2015-03-27 15:08:56 -0300

    Calculo Norte-Sur y Este-Oeste, versión preliminar

--- src/main/java/com/jsteinaker/calleslp/EsteOeste.java
+++ src/main/java/com/jsteinaker/calleslp/EsteOeste.java
@@ -17,4 +17,59 @@ public class EsteOeste extends Calle {
 			return 0;
 		}
 	}
+
+	public Resultado calcular(int calle, int altura)
+	{
+		int esquina1, esquina2;
+
+		if (altura < 300)
+		{
+			if ((calle == 51) || (calle ==53))
+			{
+				esquina1 = -calle;
+				esquina2 = -calle;
+			}
+			else
+			{
+				esquina1 = -1 * (altura / 50 -5) + 115;
+				if (esquina1 == 115)
+				{
+					esquina2 = 1;
+				}
+				else
+				{
+					esquina2 = esquina1 - 1;
+				}
+			}
+		}
+		else if (altura < 1800)
+		{
+			if ((calle == 52) && (altura < 1600))
+			{
+				esquina1 = -calle;
+				esquina2 = -calle;
+			}
+			else
+			{
+				esquina1 = altura / 50 - 5;
+				esquina2 = esquina1 + 1;
+			}
+		}
+		else
+		{
+			if ((calle == 51) || (calle ==53))
+			{
+				esquina1 = -calle;
+				esquina2 = -calle;
+			}
+			else
+			{
+				esquina1 = altura / 50 - 5 + 100;
+				esquina2 = esquina1 + 1;
+			}
+		}
+
+		return new Resultado(esquina1, esquina2);
+	}
+
 }
--- src/main/java/com/jsteinaker/calleslp/NorteSur.java
+++ src/main/java/com/jsteinaker/calleslp/NorteSur.java
@@ -17,4 +17,39 @@ public class NorteSur extends Calle {
 			return 0;
 		}
 	}
+
+	public Resultado calcular(int calle, int altura)
+	{
+		int esquina1, esquina2;
+		
+		esquina1 = altura / 50 + 32;
+		if (esquina1 > 51)
+		{
+			esquina1++;
+			if (esquina1 == 99)
+			{
+				esquina2=600;
+			}
+			else if (esquina1 > 99)
+			{
+				esquina1 += 500;
+				esquina2 = esquina1 + 1;
+			}
+			else
+			{
+				esquina2 = esquina1 + 1;
+			}
+		}
+		else if (esquina1 == 51)
+		{
+			esquina2 = 53;
+		}
+		else
+		{
+			esquina2 = esquina1 + 1;
+		}
+
+		return new Resultado(esquina1, esquina2);
+	}
+
 }
