150dea3eb14fea09e6138c3c53322d0110064e12
Javier Steinaker <jsteinaker@gmail.com>
2015-03-30 08:41:44 -0300

    Agregadas diagonales (preliminar)

---
+++ src/main/java/com/jsteinaker/calleslp/Diagonales7374.java
@@ -0,0 +1,29 @@
+package com.jsteinaker.calleslp
+
+public class Diagonales7374 extends DiagonalesPrincipales {
+
+	//Constructor
+	public Diagonales7374(int calle)
+	{
+	}
+
+	public int chequearLimites(int altura)
+	{
+		if ((altura < 1) || (altura > 31699))
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	public Resultado calcular(int calle, int altura)
+	{
+		return super.calcular(calle, altura);
+	}
+
+}
+
+
---
+++ src/main/java/com/jsteinaker/calleslp/DiagonalesPrincipales.java
@@ -0,0 +1,45 @@
+package com.jsteinaker.calleslp;
+
+public class DiagonalesPrincipales extends Calle {
+
+	//Constructor
+	public DiagonalesPrincipales(int calle)
+	{
+	}
+
+	public int chequearLimites(int altura)
+	{
+	}
+
+	public Resultado calcular(int calle, int altura)
+	{
+		int esquina1, esquina2;
+
+		if (altura < 600)
+		{
+			esquina1 = -1 * (altura / 100 - 5) + 115;
+			if (esquina1 == 115)
+			{
+				esquina2 = 1;
+			}
+			else
+			{
+				esquina2 = esquina1 - 1;
+			}
+		}
+		else if (altura < 3600)
+		{
+			esquina1 = altura / 100 - 5;
+			esquina2 = esquina1 + 1;
+		}
+		else
+		{
+			esquina1 = altura / 100 - 5 + 100;
+			esquina2 = esquina1 + 1;
+		}
+
+		return new Resultado(esquina1, esquina2);
+
+	}
+
+}
