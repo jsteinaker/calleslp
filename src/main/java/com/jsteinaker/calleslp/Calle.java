fd9a4d4340f72d4a7aaaac6e470524ada246df99
Javier Steinaker <jsteinaker@gmail.com>
2015-03-30 17:42:55 -0300

    Agregada selectora

--- src/main/java/com/jsteinaker/calleslp/Calle.java
+++ src/main/java/com/jsteinaker/calleslp/Calle.java
@@ -7,25 +7,64 @@ public abstract class Calle {
 		return calle >= menor && calle <=mayor;
 	}
 	
-	public static final Calle tipoCalle(int calle) {
-		if (entreCalles(calle, 1, 31)
-				|| entreCalles(calle, 115, 120)
-				|| entreCalles(calle, 132, 312))
+	public static final Calle tipoCalle(int calle, char checkbox)
+	{
+		if (checkbox == 'd')
 		{
-			return new NorteSur(calle);
+			switch (calle)
+			{
+				case 73:
+				case 74:
+							return new Diagonales7374(calle);
+				case 75:
+				case 76:
+							return new Diagonales7576(calle);
+				case 77:
+				case 78:
+							return new Diagonales7778(calle);
+				case 79:
+				case 80:
+							return new Diagonales7980(calle);
+				default:
+							return null;
+			}
 		}
-		else if (entreCalles(calle, 32, 99)
-				|| entreCalles(calle, 600, 708))
+		else if (checkbox == 't')
 		{
-			return new EsteOeste(calle);
-		}
-		else if (entreCalles(calle, 502, 531))
-		{
-			return new TolosaEsteOeste(calle);
+				if (entreCalles(calle, 502, 531))
+				{
+					return new TolosaEsteOeste(calle);
+				}
+				else if (entreCalles(calle, 1, 31))
+				{
+					return new TolosaNorteSur(calle);
+				}
+				else
+				{
+					return null;
+				}
 		}
 		else
 		{
-			return new TolosaNorteSur(calle);
+				if (entreCalles(calle, 1, 31)
+					|| entreCalles(calle, 115, 120)
+					|| entreCalles(calle, 132, 312))
+				{
+					return new NorteSur(calle);
+				}
+				else if (entreCalles(calle, 32, 99)
+					|| entreCalles(calle, 600, 708))
+				{
+					return new EsteOeste(calle);
+				}
+				else if (entreCalles(calle, 502, 531))
+				{
+					return new TolosaEsteOeste(calle);
+				}
+				else
+				{
+				return null;
+				}
 		}
 	}
 
