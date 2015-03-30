e2b0459087a9d352774f086b495e23591e53e5ee
Javier Steinaker <jsteinaker@gmail.com>
2015-03-30 19:39:06 -0300

    Vinculada interfaz de usuario con código interno. Compila

--- src/main/java/com/jsteinaker/calleslp/Main.java
+++ src/main/java/com/jsteinaker/calleslp/Main.java
@@ -3,6 +3,8 @@ package com.jsteinaker.calleslp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class Main extends Activity
 {
@@ -16,6 +18,36 @@ public class Main extends Activity
 
 	/** Llamada cuando se presiona el botón Calcular */
 	public void calcular(View view)
-	{
+	{	
+		char checkbox;
+		Calle calleIngresada;
+
+		// Obtenemos calle
+		EditText editText = (EditText) findViewById(R.id.edt_calle);
+		int numerocalle = Integer.parseInt(editText.getText().toString());
+
+		// Obtenemos altura
+		editText = (EditText) findViewById(R.id.edt_altura);
+		int alturacalle = Integer.parseInt(editText.getText().toString());
+
+		// Obtenemos estado Diagonal/Tolosa
+		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rbtn_tipo_direccion);
+		int idchequeado = radioGroup.getCheckedRadioButtonId();
+
+		if (idchequeado == R.id.rbtn_diagonal)
+		{
+			checkbox = 'd';
+		}
+		else if (idchequeado == R.id.rbtn_tolosa)
+		{
+			checkbox = 't';
+		}
+		else
+		{
+			checkbox = '0';
+		}
+
+		calleIngresada = Calle.tipoCalle(numerocalle, checkbox);
+
 	}
 }
