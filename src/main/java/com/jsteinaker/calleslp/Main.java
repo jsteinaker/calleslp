fadc309df5fa7c5718af94bd610567b65993d709
Javier Steinaker <jsteinaker@gmail.com>
2015-04-03 14:15:39 -0300

    Mensaje de ayuda, cambios en el layout, manejo de excepciones si el usuario no ingresa datos

--- src/main/java/com/jsteinaker/calleslp/Main.java
+++ src/main/java/com/jsteinaker/calleslp/Main.java
@@ -1,6 +1,7 @@
 package com.jsteinaker.calleslp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,77 +12,111 @@ public class Main extends Activity
 {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-
-	/** Llamada cuando se presiona el botón Calcular */
-	public void calcular(View view)
-	{	
-		char checkbox;
-		Calle calleIngresada;
-		Resultado resultado;
-
-		// Asociamos el TextView, lo necesitaremos para mostrar resultados o errores.
-		TextView textView = (TextView) findViewById(R.id.txt_resultado);
-
-		// Obtenemos calle
-		EditText editText = (EditText) findViewById(R.id.edt_calle);
-		int numerocalle = Integer.parseInt(editText.getText().toString());
-
-		// Obtenemos altura
-		editText = (EditText) findViewById(R.id.edt_altura);
-		int alturacalle = Integer.parseInt(editText.getText().toString());
-
-		// Obtenemos estado Diagonal/Tolosa
-		RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rbtn_tipo_direccion);
-		int idchequeado = radioGroup.getCheckedRadioButtonId();
-
-		if (idchequeado == R.id.rbtn_diagonal)
+		public void onCreate(Bundle savedInstanceState)
 		{
-			checkbox = 'd';
-		}
-		else if (idchequeado == R.id.rbtn_tolosa)
-		{
-			checkbox = 't';
-		}
-		else
-		{
-			checkbox = '0';
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.main);
 		}
 
-		// Al derivador, que crea un objeto de distinta clase de acuerdo al tipo de calle
-		calleIngresada = Calle.tipoCalle(numerocalle, checkbox);
-		
-		if (calleIngresada == null)
-		{
-			textView.setText("La calle ingresada no existe. Por favor, verifique los datos e intente nuevamente.");
-		}
-		else if (calleIngresada.chequearLimites(alturacalle))
-		{
-			resultado = calleIngresada.calcular(numerocalle, alturacalle);
+		/** Llamada cuando se presiona el botón Calcular */
+		public void calcular(View view)
+		{	
+			int numerocalle, alturacalle;
+			char checkbox;
+			Calle calleIngresada;
+			Resultado resultado;
 
-			// Excepciones para las calles 51, 52 y 53, que se comportan de manera distinta.
-			if ((resultado.getEsquina1() == -51) || (resultado.getEsquina1() == -53))
+			// Asociamos el TextView, lo necesitaremos para mostrar resultados o errores.
+			TextView textView = (TextView) findViewById(R.id.txt_resultado);
+
+			// Obtenemos calle
+			EditText editText = (EditText) findViewById(R.id.edt_calle);
+			try
 			{
-				textView.setText("La calle " + numerocalle + " sólo existe entre las calles 1 y 31 (numeración entre 300 y 1799).");
+				numerocalle = Integer.parseInt(editText.getText().toString());
 			}
-			else if (resultado.getEsquina1() == -52)
+			catch (NumberFormatException e)
 			{
-				textView.setText("La calle 52 no existe entre las calles 1 y 27 (numeración entre 300 y 1599).");
+				numerocalle = 0;
+			}
+
+			// Obtenemos altura
+			editText = (EditText) findViewById(R.id.edt_altura);
+			try
+			{
+				alturacalle = Integer.parseInt(editText.getText().toString());
+			}
+			catch (NumberFormatException e)
+			{
+				alturacalle = 0;
+			}
+
+			if ((numerocalle == 0) || (alturacalle == 0))
+			{
+				return;
+			}
+
+			// Obtenemos estado Diagonal/Tolosa
+			RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rbtn_tipo_direccion);
+			int idchequeado = radioGroup.getCheckedRadioButtonId();
+
+			if (idchequeado == R.id.rbtn_diagonal)
+			{
+				checkbox = 'd';
+			}
+			else if (idchequeado == R.id.rbtn_tolosa)
+			{
+				checkbox = 't';
 			}
 			else
 			{
-				textView.setText(numerocalle + " N°" + alturacalle + " queda entre " + resultado.getEsquina1() + " y " + resultado.getEsquina2());	
+				checkbox = '0';
 			}
-		}
-		else
-		{
-			textView.setText("La altura no existe en la calle especificada. Por favor, cheque los datos e intente nuevamente.");	
-		}
 
+			// Al derivador, que crea un objeto de distinta clase de acuerdo al tipo de calle
+			calleIngresada = Calle.tipoCalle(numerocalle, checkbox);
+		
+			if (calleIngresada == null)
+			{
+				textView.setText("La calle ingresada no existe. Por favor, verifique los datos e intente nuevamente.");
+			}
+			else if (calleIngresada.chequearLimites(alturacalle))
+			{
+				resultado = calleIngresada.calcular(numerocalle, alturacalle);
+
+			// Excepciones para las calles 51, 52 y 53, que se comportan de manera distinta.
+				if ((resultado.getEsquina1() == -51) || (resultado.getEsquina1() == -53))
+				{
+					textView.setText("La calle " + numerocalle + " sólo existe entre las calles 1 y 31 (numeración entre 300 y 1799).");
+				}
+				else if (resultado.getEsquina1() == -52)
+				{
+					textView.setText("La calle 52 no existe entre las calles 1 y 27 (numeración entre 300 y 1599).");
+				}
+				else
+				{
+					textView.setText(numerocalle + " N°" + alturacalle + " queda entre " + resultado.getEsquina1() + " y " + resultado.getEsquina2());	
+				}
+			}
+			else
+			{
+				textView.setText("La altura no existe en la calle especificada. Por favor, cheque los datos e intente nuevamente.");	
+			}
+
+	}
+
+	// Muestra el diálogo de ayuda
+	public void ayuda(View view)
+	{
+		// View vistaAyuda = getLayoutInflater().inflate(R.layout.ayuda, null, false);
+		
+		AlertDialog.Builder dlg_ayuda = new AlertDialog.Builder(this);
+		// dlg_ayuda.setView(vistaAyuda);
+		dlg_ayuda.setTitle("Ayuda");
+		dlg_ayuda.setMessage(R.string.txt_ayuda);
+		dlg_ayuda.setPositiveButton("OK", null);
+		dlg_ayuda.create();
+		dlg_ayuda.show();
 	}
 
 }
